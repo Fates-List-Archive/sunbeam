f1ba011cf551aab7edf0c73d1d5c140242fb2aed
cheesycod <cheesycod@outlook.com>
2021-12-30 18:23:25 +0000

    fix

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -1,5 +1,10 @@
+import { browser } from '$app/env';
 export async function fetchFates(url: string) {
     let res = null
+    if(browser || process.env.NODE_VERSION) {
+        // Always use direct if browser
+        return await fetch("https://fateslist.xyz"+url)
+    }
     try {
         res = await fetch("http://127.0.0.1:9999"+url)
     } catch (err) {
