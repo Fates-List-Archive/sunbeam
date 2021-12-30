af35209846c23cf7bb70aa9d426eb9a94af2a656
cheesycod <cheesycod@outlook.com>
2021-12-30 15:59:17 +0000

    fix

--- src/lib/cards/CardContainer.svelte
+++ src/lib/cards/CardContainer.svelte
@@ -6,11 +6,13 @@
   .container {
     display: flex;
     flex-wrap: wrap;
+    align-items: center;
     padding-left: 0;
   }
   
   slot {
     list-style: none;
     flex: 0 0 33%;
+    justify-content: center;
   }  
 </style>
