6c81541ab177a7c89b5e0221cccfd091b2263fca
cheesycod <cheesycod@outlook.com>
2021-12-31 09:08:46 +0000

    f

--- src/routes/bot/[id]/invite.svelte
+++ src/routes/bot/[id]/invite.svelte
@@ -4,9 +4,13 @@
     export async function load({ params, fetch, session, stuff }) {
         let inviteUrl = await fetchFates(`/api/v2/bots/${params.id}/_sunbeam/invite`)
         let inviteJson = await inviteUrl.json()
+        
+        // JS and URLS do not go well together
+        console.log(inviteJson, decodeURIComponent(inviteJson.invite))
         return {
-            status: 302,
-            redirect: inviteJson.invite
+            status: 307,
+            redirect: decodeURIComponent(inviteJson.invite)
+            //redirect: `https://fateslist.xyz/api/v2/_sunbeam/redirect?id=${inviteJson.fallback}`
         };
     }
 </script>
