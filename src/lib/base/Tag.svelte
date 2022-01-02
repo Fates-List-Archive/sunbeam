935710d7a442c21cb0e6527e804b99098a066f2b
cheesycod <cheesycod@outlook.com>
2022-01-02 06:01:57 +0000

    tags

--- src/lib/base/Tag.svelte
+++ src/lib/base/Tag.svelte
@@ -1,50 +1,54 @@
 <script lang="ts">
-import Icon from '@iconify/svelte';
-import Button from "@smui/button";
-import type { TagInterface } from "$lib/types"
-export let tags: TagInterface[];
-export let targetType: string;
-</script>
-<div class="tag-container">
-    {#each tags as tag}
-        <Button id="tags-{tag.id}" class="tag-item" href="/frostpaw/search/tags?tag={tag.id}&target_type={targetType}" title="{tag.name}" touch variant="outlined" rel="external">
-            <Icon class="white" icon="{tag.iconify_data}" inline={false} aria-hidden="true"></Icon>
+    import Icon from '@iconify/svelte';
+    import Button from "@smui/button";
+    import type { TagInterface } from "$lib/types"
+    export let tags: TagInterface[];
+    export let targetType: string;
+    </script>
+    <div class="tag-container">
+        {#each tags as tag}
             <span>
-                <strong>{tag.name}</strong>
+                <Button id="tags-{tag.id}" class="tag-item" href="/frostpaw/search/tags?tag={tag.id}&target_type={targetType}" title="{tag.name}" touch variant="outlined" rel="external">
+                    <Icon class="white tag-icon" icon="{tag.iconify_data}" inline={false} aria-hidden="true"></Icon>
+                    <strong>{tag.name}</strong>
+                </Button>
             </span>
-        </Button>
-    {/each}
-</div>
+        {/each}
+    </div>
+    
+    <style lang="scss">
+        :global(.tag-icon) {
+            font-size: 15px;
+            margin-right: 5px;
+        }
 
-<style lang="scss">
-    span {
-        font-size: 13px;
-    }
+        span {
+            font-size: 13px;
+        }
 
-    :global(.tag-item) {
-        opacity: 1 !important;
-	    font-weight: bold; 
-	    border: solid thin !important; 
-	    outline: none !important; 
-        color: white !important;
-        font-size: 18px;
-	    display: inline-block;
-	    margin: 10px;
-	    //width: 170px;
-	    text-overflow: ellipsis;
-	    word-wrap: break-word;
-	    cursor: pointer;
-    }
-    .tag-container {
-	    height: 100%;
-	    width: 85%;
-	    margin-left: auto;
-	    margin-right: auto;
-	    overflow-x: scroll;
-	    overflow-y: hidden;
-	    white-space: nowrap;
-	    line-height: 13px;
-	    margin-top: 5px;
-    }
-
-</style>
+        :global(.tag-item) {
+            opacity: 1 !important;
+            color: white !important;
+            border: solid thin !important; 
+            outline: none !important; 
+            word-wrap: break-word;
+            text-overflow: ellipsis;
+            margin-left: 10px;
+            margin-right: 10px;
+            cursor: pointer;
+            min-width: 180px;
+        }
+    
+        .tag-container {
+            height: 100%;
+            width: 90%;
+            margin-left: auto;
+            margin-right: auto;
+            overflow-x: scroll;
+            overflow-y: hidden;
+            white-space: nowrap;
+            line-height: 13px;
+            margin-top: 5px;
+        }
+    
+    </style>
