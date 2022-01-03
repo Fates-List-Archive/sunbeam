8a9a279c647a71c284ce2eea8f29abe958b64d43
cheesycod <cheesycod@outlook.com>
2022-01-03 07:30:23 +0000

    fix

--- src/routes/api/[...request].ts
+++ src/routes/api/[...request].ts
@@ -2,27 +2,39 @@ async function proxy(request: any, method: string) {
     let proxiedURL = request.url.href.replace("sunbeam.fateslist.xyz", "fateslist.xyz").replace("sunbeam-cf.fateslist.xyz", "fateslist.xyz")
     let headers = {
         'content-type': 'application/json', 
+        'Accept': 'application/json',
         "Frostpaw": "0.1.0", 
         "method": method
     }
 
     for (const property in request.headers) {
-        if(property === "host" || property === "connection") {
+        if(property === "host" || property === "connection" || property == "accept") {
             continue
         } else {
             headers[property.toLowerCase()] = request.headers[property]
         }
     }
 
+    let body = JSON.stringify(request.body)
+
     let res = await fetch(proxiedURL, {
         method: method,
         headers: headers,
-        body: JSON.stringify(request.body)
+        mode: 'cors',
+        credentials: 'include',
+        body: body
     })
 
     let response = {
         status: res.status,
     }
+
+    let resHeaders = {}
+
+    for(const property in res.headers) {
+        resHeaders[property] = res.headers[property]
+    }
+
     if(method !== "HEAD") {
         response["body"] = await res.json()
         response["body"]["new_api_url"] = "https://api.fateslist.xyz"
@@ -30,7 +42,8 @@ async function proxy(request: any, method: string) {
             headers: headers,
             method: method,
             data: request.body,
-            dataStr: JSON.stringify(request.body)
+            data_str: body,
+            resp_headers: resHeaders
         }
     }
     return response
