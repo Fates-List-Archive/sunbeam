20af3e542d317861b5dab50555204c1701ff7133
cheesycod <cheesycod@outlook.com>
2022-01-04 13:22:25 +0000

    Add Error Page and invite in prep for launch

---
+++ src/routes/__error.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	/** @type {import('@sveltejs/kit').ErrorLoad} */
+	export function load({ error, status }) {
+		return {
+			props: {
+                status: status,
+                error: error.message
+			}
+		};
+	}
+</script>
+<script>
+	export let status;
+    export let error;
+</script>
+
+<h1 style="text-align: center">{status}</h1>
+<h2>{error}</h2>
---
+++ src/routes/server/[id]/invite.svelte
@@ -0,0 +1,31 @@
+<script context="module">
+    export const prerender = false;
+    export async function load({ params, fetch, session, stuff }) {
+        let auth = ""
+        if(session.session.user) {
+            auth = `${session.session.user.id}|${session.session.token}`
+        }
+        let inviteUrl = await fetch(`https://fateslist.xyz/api/v2/guilds/${params.id}/_sunbeam/invite`, {
+            method: "GET",
+            headers: {
+                "Frostpaw": "0.1.0",
+                "Frostpaw-Auth": auth
+            }
+        })
+
+        let inviteJson = await inviteUrl.json()
+
+        if(!inviteUrl.ok) {
+            return {
+                status: 400,
+                error: new Error(inviteJson.reason)
+            }
+        }
+        
+        // JS and URLS do not go well together
+        return {
+            status: 307,
+            redirect: inviteJson.invite
+        };
+    }
+</script>
