9697310cbc8330ae1236f1f183f5cb70986c427d
Rootspring <66250972+cheesycod@users.noreply.github.com>
2022-01-07 07:21:33 +0530

    Create _riverclan.svelte

---
+++ src/routes/frostpaw/_riverclan.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { loginUser } from "$lib/request"
+  import { browser } from '$app/env';
+  let msg = "Please wait..."
+  function riverLogin() {
+      if(!browser) {
+          return
+      }
+      const urlParams = new URLSearchParams(window.location.search);
+      let redirect = urlParams.get("redirect")
+      if(!redirect || !redirect.startsWith("https://api.fateslist.xyz")) {
+          msg = "Invalid redirect"
+      }
+      localStorage.sunbeamLogin = redirect
+      loginUser(true)
+  }
+  riverLogin()
+</script>
+{msg}
