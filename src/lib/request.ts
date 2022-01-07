849b9a9276a68ad41a744cb7868c095bf6ac2a5b
Rootspring <66250972+cheesycod@users.noreply.github.com>
2022-01-08 01:05:14 +0530

    Update request.ts

--- src/lib/request.ts
+++ src/lib/request.ts
@@ -3,9 +3,13 @@ import { browser } from '$app/env';
 // Change this if cf will ever be used
 const usingCf = true
 
-export async function fetchFates(url: string) {
+export async function fetchFates(url: string, auth: string) {
     // Always use direct if browser
-    return await fetch("https://api.fateslist.xyz"+url, {headers: {"Frostpaw": "0.1"}})
+    headers = {"Frostpaw": "0.1"}
+    if(auth) {
+        headers["Frostpaw-Auth"] = auth
+    }
+    return await fetch("https://api.fateslist.xyz"+url, {headers: headers})
 }
 
 export async function roll(type: string) {
