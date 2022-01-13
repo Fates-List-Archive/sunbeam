92bda6e96bcfb4a63b7ba8a16d45e5fd88036618
cheesycod <cheesycod@outlook.com>
2022-01-13 09:23:51 +0000

    update version

--- src/routes/frostpaw/about.svelte
+++ src/routes/frostpaw/about.svelte
@@ -2,6 +2,6 @@
 <p>Sunbeam Version: {version}</p>
 <p>Frostpaw Client Version: {frostpawVersion}</p>
 <script lang="ts">
-    export let version: string = "3"
+    export let version: string = "4"
 	export let frostpawVersion: string = "0.1.0"
 </script>
