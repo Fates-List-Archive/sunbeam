a513028c5d754259cbe485e08397a93487951ec8
cheesycod <cheesycod@outlook.com>
2022-01-14 11:15:36 +0000

    add add bot page

---
+++ src/routes/frostpaw/add-bot.svelte
@@ -0,0 +1,15 @@
+<iframe src="https://api.fateslist.xyz/bot/admin/add?iframe=true" width="100%"></iframe>
+<style>
+	iframe {
+		border: none;
+		height: 100vh;
+	}
+</style>
+<script lang="ts">
+	import { loginUser } from "$lib/request"
+	import { session } from '$app/stores';
+	let token = $session.session.token;
+	if(!token) {
+		loginUser()
+	}
+</script>
