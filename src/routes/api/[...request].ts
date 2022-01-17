7158b5ef66494d9fbad0268779af776689c29c75
cheesycod <cheesycod@outlook.com>
2022-01-17 13:46:27 +0000

    remove legacy proxy in favor of 307

--- src/routes/api/[...request].ts
+++ src/routes/api/[...request].ts
@@ -1,59 +1,12 @@
 // Simple request proxy
 async function proxy(request: any, method: string) {
     let proxiedURL = request.url.href.replace("sunbeam.fateslist.xyz", "fateslist.xyz").replace("sunbeam-cf.fateslist.xyz", "fateslist.xyz").replace("fateslist.xyz", "api.fateslist.xyz")
-    let headers = {
-        'content-type': 'application/json', 
-        'Accept': 'application/json',
-        "Frostpaw-Proxy": "0.1.0", 
-        "method": method
-    }
-
-    for (const property in request.headers) {
-        if(property === "host" || property === "connection" || property == "accept" || property.includes("cookie")) {
-            continue
-        } else {
-            headers[property.toLowerCase()] = request.headers[property]
-        }
-    }
-
-    let body = JSON.stringify(request.body)
-
-    let fetchData = {
-    	method: method,
-	headers: headers
-    }
-
-    if(method != "GET" && method != "HEAD") {
-    	fetchData[body] = body
-    }
-
-    let res = await fetch(proxiedURL, fetchData)
 
     let response = {
-        status: res.status,
+        status: 307,
+        headers: {"Location": proxiedURL}
     }
 
-    let resHeaders = {}
-
-    //for(const property in res.headers) {
-    //    resHeaders[property] = res.headers[property]
-    //}
-   	
-    if(proxiedURL.includes("redoc") || proxiedURL.includes("widget") || proxiedURL.includes("swagger") || proxiedURL.includes(".js")) {
-    	return {headers: {"Location": proxiedURL}, status: 301}
-    }
-
-    if(method !== "HEAD") {
-	response["body"] = await res.json()
-	response["body"]["new_api_url"] = "https://api.fateslist.xyz"
-        response["body"]["_proxy"] = {
-            headers: headers,
-            method: method,
-            data: request.body,
-            data_str: body,
-            resp_headers: resHeaders
-        }
-    }
     return response
 }
 
