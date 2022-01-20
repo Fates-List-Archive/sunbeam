393be824f4c447e1d849cf45ee40bebff151153b
cheesycod <cheesycod@outlook.com>
2022-01-20 14:46:31 +0000

    Add stats URL

---
+++ src/routes/frostpaw/stats.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import PageFrame from "$lib/pages/Iframe.svelte"
+</script>
+<PageFrame 
+	iframeUrl="https://api.fateslist.xyz/_sunbeam/pub/stats"
+	title="Add Bot Page" 
+	metaTitle="Fates List | Stats"
+	metaUrl="https://fateslist.xyz/frostpaw/stats"
+></PageFrame>
