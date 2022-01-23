4bd0748f08203dea853e6a0617f2db9b3e9b8442
cheesycod <cheesycod@outlook.com>
2022-01-23 14:02:35 +0000

    new settings page

--- src/routes/bot/[id]/settings.svelte
+++ src/routes/bot/[id]/settings.svelte
@@ -1,21 +1,44 @@
-<script context="module" lang="ts">
-	export const prerender = false;
-	/** @type {import('@sveltejs/kit@next').Load} */
-	export async function load({ params, fetch, session, stuff }) {
-        return {
-            props: {
-                id: params.id
-            }
-        }
-    }
+<script lang="ts" context="module">
+	import { fetchFates } from "$lib/request";
+		/** @type {import('@sveltejs/kit@next').Load} */
+		export async function load({params, fetch, session, stuff}) {
+			if(!session.session.token) {
+				return {
+					props: {
+						failed: true,
+						html: ""
+					}
+				}
+			}
+			let url = `/api/v2/_sunbeam/bots/${params.id}/settings?user_id=${session.session.user.id}`
+			let res = await fetchFates(url, "User " + session.session.token, fetch)
+			if(!res.ok) {
+				let json = await res.json()
+				return {
+					status: res.status,
+					error: new Error(`${JSON.stringify(json)}`)
+				}
+			}
+			let json = await res.json()
+			return {
+				props: {
+					failed: false,
+					html: json.html
+				}
+			}
+		}
 </script>
 <script lang="ts">
-	import PageFrame from "$lib/pages/Iframe.svelte"
-    export let id: string;
+	import { browser } from "$app/env"
+	import { loginUser } from "$lib/request"
+	export let failed: boolean;
+	export let html: string;
+	if(failed) {
+		if(browser) {
+			localStorage.reloadIgnore = window.location.href
+			localStorage.reloadPage = "1"
+			loginUser(false)
+		}
+	}
 </script>
-<PageFrame 
-	iframeUrl="https://api.fateslist.xyz/_sunbeam/pub/bot/{id}/settings" 
-	title="Add Bot Page" 
-	metaTitle="Fates List | Bot Settings"
-	metaUrl="https://fateslist.xyz/bot/{id}/settings"
-></PageFrame>
+{@html html}
