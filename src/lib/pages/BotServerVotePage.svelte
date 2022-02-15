5ff092896e1ba56d8941095e233480acaa09c014
cheesycod <cheesycod@outlook.com>
2022-02-15 07:07:55 +0000

    Add long-description id

--- src/lib/pages/BotServerVotePage.svelte
+++ src/lib/pages/BotServerVotePage.svelte
@@ -125,7 +125,7 @@
 
 {@html data.css}
 
-<div class="lozad bot-page-banner" data-background-image="{data.banner}">
+<div id="long-description" class="lozad bot-page-banner" data-background-image="{data.banner}">
     <img class="bot-avatar" loading="lazy" src="{data.user.avatar.replace(".png", ".webp").replace("width=", "width=120px")}" id="bot-avatar" alt="{data.user.username}'s avatar">
     <article class="bot-page">
         <a href="/{type}/{data.user.id}/invite" class="banner-decor bot-username bot-username-link">
