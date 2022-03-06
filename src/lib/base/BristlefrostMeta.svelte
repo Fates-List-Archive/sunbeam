f19244ef87249cea618ab41ff4cd50a121e4ceb0
cheesycod <cheesycod@outlook.com>
2022-03-06 06:52:02 +0000

    remove canonical for now

--- src/lib/base/BristlefrostMeta.svelte
+++ src/lib/base/BristlefrostMeta.svelte
@@ -21,5 +21,4 @@
 	<meta property="twitter:title" content="{title}">
 	<meta property="twitter:description" content="{description}">
 	<meta property="twitter:image" content="{thumbnail}">
-	<link rel="canonical" href='{url + "/" || "https://fateslist.xyz/"}'>
 </svelte:head>
