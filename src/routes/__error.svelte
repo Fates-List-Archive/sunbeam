f76a06e82cd86af454824e7cc1f1c5d58e74ec93
cheesycod <cheesycod@outlook.com>
2022-03-18 05:18:46 +0000

    add server maint error handling

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,10 +1,20 @@
 <script context="module">
 	/** @type {import('@sveltejs/kit').ErrorLoad} */
 	export function load({ error, status }) {
+		if(status == 408 || status == 502) {
+			return {
+				props: {
+					title: "Server Maintenance",
+					message: "The Fates List API is currently under maintenance.<br/>Dont worry this won't take long!",
+					serverMaint: true
+				}
+			}
+		}
 		return {
 			props: {
                 status: status,
-                error: error.message
+                error: error.message,
+				serverMaint: false
 			}
 		};
 	}
@@ -12,12 +22,17 @@
 <script>
 	export let status;
     export let error;
+	export let title = "";
+	export let message = "";
+	export let serverMaint = false;
 </script>
 
-<h1 style="text-align: center">{status}</h1>
-<h2>{error}</h2>
+{#if serverMaint}
+	<h1>{title}</h1>
+	<h2>{@html message}</h2>
+{:else}
+	<h1 style="text-align: center">{status}</h1>
+	<h2>{error}</h2>
 
-{#if status == 408}
-	<h3>Server down for maintenance! Don't worry, this won't take too long!</h3>
+	<p>Please visit our <a href="https://fateslist.xyz/server/789934742128558080/invite">support server</a> if you have any queries or concerns or just for fun!</p>
 {/if}
-<p>Please visit our <a href="https://fateslist.xyz/server/789934742128558080/invite">support server</a> if you have any queries or concerns or just for fun!</p>
