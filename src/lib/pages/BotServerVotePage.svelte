fdd8d041a674d9472510335a1683399f21e92720
cheesycod <cheesycod@outlook.com>
2022-04-26 09:16:51 +0000

    move about to seperate component

--- src/lib/pages/BotServerVotePage.svelte
+++ src/lib/pages/BotServerVotePage.svelte
@@ -133,44 +133,7 @@
         </a>
         <div class="bot-page-content">
             <div class="accordion-container">
-                <Accordion multiple>
-                    {#if enums.helpers.flagCheck(enums.Flags.system, data.flags) }
-                    <Panel color="secondary" open>
-                        <Header ripple={false}>
-                          System Bot
-                        </Header>
-                        <Content class="accordian-container">
-                            This bot is a system bot and was added as a placeholder to avoid malicious use of your bot. 
-                            If you are the bots owner, please either edit your bot setting System to False if available or contact 
-                            Fates List Support to claim the bot. To avoid misinformation and false advertising, the actual owner of 
-                            the bot is listed in About who may edit their bot if they wish to change anything on it.
-
-			    <em>System gots cannot be voted for until claimed</em>
-                        </Content>
-                    </Panel>
-                    {/if}
-                    {#if data.state == enums.BotState.denied}
-                    <Panel color="primary" open>
-                        <Header ripple={false}>
-                          Bot Denied
-                        </Header>
-                        <Content class="accordian-container">
-                            This bot has been DENIED from this list for being a 
-                            low-quality bot. Please do not invite it.
-                        </Content>
-                    </Panel>
-                    {:else if data.state == enums.BotState.banned}
-                    <Panel color="primary" open>
-                        <Header ripple={false}>
-                          Bot Banned
-                        </Header>
-                        <Content class="accordian-container">
-                            This bot has been BANNED from this bot list for violating our rules or being a low-quality bot. 
-                            Please do not invite it until further notice!
-                        </Content>
-                    </Panel>
-                    {/if}
-                </Accordion>
+                <Warns data={data}></Warns>
             </div>
             <p class="banner-decor white" id="bot-description">{@html data.description.replace("p>", "span>") }</p>
             <div class="buttons">
@@ -188,16 +151,15 @@
 </div>
 
 <script lang="ts">
-    import Accordion, { Panel, Header, Content } from '@smui-extra/accordion';
     import BristlefrostMeta from "$lib/base/BristlefrostMeta.svelte";
     import Icon from '@iconify/svelte';
     import Button from '@smui/button';
-    import { enums } from '../enums/enums';
     import { voteHandler } from '$lib/request'
     import { session } from '$app/stores';
 import alertstore from '$lib/alertstore';
 	export let data: any;
     export let type: string;
+    import Warns from "./helpers/Warns.svelte"
 
     async function voteBot() {
         let token = $session.session.token
