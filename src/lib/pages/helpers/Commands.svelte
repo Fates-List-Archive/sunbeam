04083478ec862782bb1f1f3e5c0b6a49a16614d7
cheesycod <cheesycod@outlook.com>
2022-04-26 07:48:26 +0000

    remove useless showHide code

--- src/lib/pages/helpers/Commands.svelte
+++ src/lib/pages/helpers/Commands.svelte
@@ -9,42 +9,6 @@
         return str.replaceAll("_", " ").replace(/(^|\s)\S/g, function(t) { return t.toUpperCase() });
     }
 
-	function fade(element) {
-    	var op = 1;  // initial opacity
-    	var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    	}, 10);
-	}
-	function unfade(element) {
-    	var op = 0.1;  // initial opacity
-    	element.style.display = 'table';
-    	var timer = setInterval(function () {
-        	if (op >= 1){
-            	clearInterval(timer);
-        	}
-        	element.style.opacity = op;
-        	element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        	op += op * 0.1;
-    	}, 10);
-    }
-
-	function showHideCommandGroup(cmdGroup: string) {
-		let id = `#${cmdGroup}-table`
-		let group = document.querySelector(id)
-		if(group.style.display != 'none') {
-			fade(group)
-		}
-		else {
-			unfade(group)
-		}
-	}
-
     let groupedCmds = {}
     let groups = {}
 
