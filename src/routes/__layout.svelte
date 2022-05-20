631ba7140956abd4f1cad8351c96ecc122923414
cheesycod <cheesycod@outlook.com>
2022-05-20 11:54:59 +0000

    remove alert

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -74,22 +74,6 @@
 			}
 			$alertstore = opt;
 		};
-
-		// Temporary Alert
-		window.alert({
-			title: 'Login',
-			message: 'Please enter your Username!',
-			id: 1030404,
-			type: 7,
-			input: {
-				label: 'Username',
-				placeholder: 'Username',
-				multiline: true,
-				function: (value) => {
-					console.log(value);
-				}
-			}
-		});
 	}
 </script>
 
