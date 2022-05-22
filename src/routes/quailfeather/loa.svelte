98bf4b07dc1c6d2d5837d10f178f21e66c2240b8
cheesycod <cheesycod@outlook.com>
2022-05-22 14:31:48 +0000

    remove staff verify note from loa

--- src/routes/quailfeather/loa.svelte
+++ src/routes/quailfeather/loa.svelte
@@ -48,8 +48,7 @@ import QuailTree from "./_helpers/QuailTree.svelte";
         })
 
         if(res.ok) {
-            let data = await res.json();
-            alert(`Staff verified! Your lynx password is ${data.pass}`);
+            alert(`Successfully sent LOA`);
         }
     }
 </script>
